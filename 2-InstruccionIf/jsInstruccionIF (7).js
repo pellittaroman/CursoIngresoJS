f186fab90ab2dc27bcf93967a90c6e7bc569b646
pellittaroman <36174240+pellittaroman@users.noreply.github.com>
2018-02-06 22:22:19 -0300

    ejercicio

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,17 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var estadocivil
+	var edad
+	edad= document.getElementById('edad').value;
+	estadocivil= document.getElementById('estadoCivil').value;
+	if(edad<18)
+	{
+		if(estadocivil!="Soltero")
+		{
+			alert("Es muy pequeño para No ser soltero");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
