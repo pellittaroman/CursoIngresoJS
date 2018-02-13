59089cc36c66b22c15cf0e0c66fb0fe338e3c5d9
pellittaroman <36174240+pellittaroman@users.noreply.github.com>
2018-02-13 14:36:02 -0300

    ejercios resueltos

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
+edad= document.getElementById('edad').value;
 
+if (edad<13) 
+{
+	alert("Usted es un niño");
+}else
+{
+	if (edad>12 && edad<18) 
+	{
+		alert("Usted es un adolecente");
+	}else{
+			alert ("Usted es mayor");
+	}
+}
 
 
 
