1a5322a7dd400ad3d953f992ea2bcb5be9ba53ef
pellittaroman <36174240+pellittaroman@users.noreply.github.com>
2018-02-17 12:06:55 -0300

    ejercicios complentos
    del 1 al 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,29 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)
+{
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	alert("Falta para el invierno");
+	break;
+
+	case "Julio":
+	case "Agosto":
+	alert("Abrigate que hace frio");
+	break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	alert("Ya pasamos el, ahora calor");
+	break;
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("Tiene 28 dias");
+		break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		alert("Tiene 30 dias");
+		break;
+		default:
+		alert ("Tiene 31 dias")
+		break;
+	}
 	
 
 
